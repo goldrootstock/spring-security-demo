2add623e131d1c44d328a2c0905463f0d746d25a
juwonkim <goldrootstock@gmail.com>
2021-10-06 14:03:41 +0900

    LoginConroller Created

--- src/main/java/com/goldrootstock/springsecurity/demo/controller/LoginConroller.java
+++ src/main/java/com/goldrootstock/springsecurity/demo/controller/LoginConroller.java
@@ -1,5 +1,14 @@
 package com.goldrootstock.springsecurity.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginConroller {
+	
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		return "plain-login";
+	}
 
 }
